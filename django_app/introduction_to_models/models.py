135d8c3bae7b40bf2d6596620c3aa3f2dd4f245e
ym4199 <ym4199@gmail.com>
2017-06-05 12:52:00 +0900

    modify Person model(choices, recursive foreign key), add car and Manufacturer model

--- django_app/introduction_to_models/models.py
+++ django_app/introduction_to_models/models.py
@@ -1,12 +1,55 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Person(models.Model):
     SHIRT_SIZES = (
-        ('S','Small'),
-        ('M','Medium'),
-        ('L','Large'),
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
     )
-    name = models.CharField(max_length=60)
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+    PERSON_TYPES = (
+        ('student', '학생'),
+        ('teacher', '선생'),
+    )
+    person_type = models.CharField(
+        '유형',
+        max_length=10,
+        choices=PERSON_TYPES,
+        default=PERSON_TYPES[0][0]
+    )
+    teacher = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    name = models.CharField('name', max_length=60)
+    shirt_size = models.CharField(
+        'size',
+        max_length=1,
+        choices=SHIRT_SIZES,
+        help_text='남자는 L 쓰세요',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=40)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
