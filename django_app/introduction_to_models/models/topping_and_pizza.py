a9a9cf840013cb9e05ad2e31fa25b7cf390556a2
ym4199 <ym4199@gmail.com>
2017-06-05 16:20:54 +0900

    modify pizza __str__

--- django_app/introduction_to_models/models/topping_and_pizza.py
+++ django_app/introduction_to_models/models/topping_and_pizza.py
@@ -15,7 +15,19 @@ class Pizza(models.Model):
     toppings = models.ManyToManyField(Topping)
 
     def __str__(self):
-        return self.name
+        # topping_string=''
+        # for topping in self.toppings.all():
+        #     topping_string += topping.name
+        #     topping_string += ', '
+        # toppings_string = topping_string[:-2]
+        # return '{} ({})'.format(
+        #     self.name,
+        #     toppings_string
+        # )
+        #
+        # [topping for topping in self.toppings.all()]
+        topping_string = ', '.join([topping.name for topping in self.toppings.all()])
+        return '{}({})'.format(self.name, topping_string)
 
     class Meta:
         ordering = ('name',)
