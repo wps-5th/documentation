304654b851bb57c1027abfdbe472c992ca6a0b9a
ym4199 <ym4199@gmail.com>
2017-06-06 23:10:19 +0900

    add test

--- django_app/introduction_to_models/models/intermediate.py
+++ django_app/introduction_to_models/models/intermediate.py
@@ -1,22 +1,72 @@
+from datetime import date, timezone
 from django.db import models
+
 
 class Player(models.Model):
     name = models.CharField(max_length=40)
 
     def __str__(self):
         return self.name
+        # current_club 프로퍼티에 현재 속하는 Club 리턴
+        # current_tradeinfo 프로터피에 현재 자신의 TradeInfo 리턴
+
+    @property
+    def current_club(self):
+        return self.club_set.get(tradeinfo__date_leaved__isnull=True).name
+
+    # date_leaved=None 을 통해 최근 팀에서 뛰고 있는 선수들을 찾는다. (최근 정보를 원함으로 과거 이적현황을 알 필요 없다)
+    @property
+    def current_tradeinfo(self):
+        return TradeInfo.objects.get(
+            player__name=self,
+            date_leaved=None,
+        )
+
 
 class Club(models.Model):
     name = models.CharField(max_length=40)
     players = models.ManyToManyField(
         Player,
         through='TradeInfo',
+        through_field=('player', 'club')
     )
+
     def __str__(self):
         return self.name
+
+    # club과 year를 매개변수로 받지만 year 값이 없다면 팀에 합류한 시점이 현재보다 이전인 그리고 이적을 가지 않은 상태의 선수를 본다.
+    # year 값이 주어진다면 해당 년도의 마지막 날까지 합류한 팀의 선수를 확인한다.
+    def squad(self, year=None):
+        if year == None:
+            
+            TradeInfo.objects.filter(date_leaved__insull=True, date_joined__lte=timezone.now())
+            return
+        else:
+            return TradeInfo.objects.filter(date_joined__gte=timezone.datetime(year, 12, 31)).name
+            # squad 메서드에 현직 선수들만 리턴
+            # 인수로 년도(2017, 2015 ...)을 받아 해당년도의 현직 선수들을 리턴
+            # 주어지지 않으면 현재를 기준으로 함
+
 
 class TradeInfo(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     club = models.ForeignKey(Club, on_delete=models.CASCADE)
     date_joined = models.DateField()
     date_leaved = models.DateField(null=True, blank=True)
+
+    prev_club = models.ForeignKey(
+        Club,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    recommender = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    # property 로 is_current 속성이 TradeInfo가 현재 현직(leave 하지 않았는지) 여부 반환
+    # recommender와 prev_club을 활성화시키고 Club의 MTM필드에 through_fields를 명시
--- django_app/introduction_to_models/tests.py
+++ django_app/introduction_to_models/tests.py
@@ -1,3 +1,18 @@
+from datetime import date
 from django.test import TestCase
 
 # Create your tests here.
+from introduction_to_models.models import Player, Club, TradeInfo
+
+
+class Inter(TestCase):
+    def test_inter(self):
+        a = Player.objects.create(name='bale')
+        # c = Player.objects.create(name='nnn')
+        b = Club.objects.create(name='tottenham')
+        TradeInfo.objects.create(player=a, club=b, date_joined=date(2000, 3, 3))
+        # TradeInfo.objects.create(player=c, club=b, date_joined=date(2000,3,3), date_leaved=date(2010,1,1))
+        self.assertEqual(a.current_club,'tottenham')
+        # self.assertEqual(c.current_club,'tottenham')
+        self.assertEqual(a.current_tradeinfo,'tottenham')
+        # self.assertEqual(c.current_tradeinfo)
